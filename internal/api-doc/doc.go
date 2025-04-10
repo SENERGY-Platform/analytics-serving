556f9cf865347f0a78c18af27314f6f888792783
y-du <42994541+y-du@users.noreply.github.com>
2025-04-10 15:11:35 +0200

    add publish asyncapi doc

---
+++ internal/api-doc/doc.go
@@ -0,0 +1,26 @@
+package api_doc
+
+import (
+	"context"
+	"github.com/SENERGY-Platform/api-docs-provider/lib/client"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func PublishAsyncapiDoc() {
+	adpClient := client.New(http.DefaultClient, os.Getenv("API_DOCS_PROVIDER_BASE_URL"))
+	file, err := os.Open("docs/asyncapi.json")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	ctx, cf := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cf()
+	err = adpClient.AsyncapiPutDocFromReader(ctx, "github_com_SENERGY-Platform_analytics-serving", file)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
